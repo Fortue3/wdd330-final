8eef54011d1a234718863a227f08b2d1fc521afd
Fortue3 <kwe22002@byui.edu>
2024-06-04 19:31:04 +0200

    adding

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ function fetchCategories() {
         .then(data => {
             const categories = data.categories;
             let output = '';
+            // console.log("categories : ", categories)
             categories.forEach(category => {
                 output += `
-                    <div class="category" onclick="window.location.href='recipes.html?category=${category.strCategory}'">
-                        <img src="${category.strCategoryThumb}" alt="${category.strCategory}">
-                        <h3>${category.strCategory}</h3>
-                    </div>
+                <div class="category" onclick="window.location.href='recipes.html?category=${category.strCategory}'">
+                ${category.strCategory}</div>
+
                 `;
             });
             document.getElementById('categories').innerHTML = output;
@@ -36,10 +36,20 @@ function fetchRecipes(category) {
             let output = '';
             recipes.forEach(recipe => {
                 output += `
-                    <div class="recipe">
+                    <div class="card">
                         <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
-                        <h3>${recipe.strMeal}</h3>
-                        <button onclick="saveRecipe(${recipe.idMeal})">Save Recipe</button>
+                        <div class="card-content ">
+                            <h3>${recipe.strMeal}</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                            <div class="btns">
+                                <button class="button" onclick="window.location.href='recipe.html?id=${recipe.idMeal}'">
+                                    <i class="fas fa-eye"></i>View
+                                </button>
+                                <button class="button" onclick="saveRecipe(${recipe.idMeal})">
+                                    <i class="fas fa-bookmark"></i>Save Recipe
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 `;
             });
@@ -53,29 +63,53 @@ function fetchRecipeDetails(id) {
         .then(data => {
             const recipe = data.meals[0];
             let ingredients = '';
+            let ingredients1 = '';
             for (let i = 1; i <= 20; i++) {
                 if (recipe[`strIngredient${i}`]) {
                     ingredients += `<li>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}</li>`;
+                    ingredients1 += `<tr><td>${recipe[`strIngredient${i}`]} - ${recipe[`strMeasure${i}`]}<td></tr>`
                 } else {
                     break;
                 }
             }
-            const output = `
+            const details = `
+            <div class="right-box">
+                <div class="main-image-box" id="mainImageBox">
+                    <img src="${recipe.strMealThumb}" id="mainImage" class="main-image" alt="">
+                </div>
+            </div>
+
+            <div class="details-box">
                 <h1>${recipe.strMeal}</h1>
-                <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
                 <h2>Ingredients</h2>
                 <ul>${ingredients}</ul>
                 <h2>Instructions</h2>
                 <p>${recipe.strInstructions}</p>
                 <button onclick="saveRecipe(${recipe.idMeal})">Save Recipe</button>
+            </div>
             `;
-            document.getElementById('recipe-details').innerHTML = output;
+
+            const output = `
+                <div class="card">
+                    <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
+                    <div class="card-content ">
+                        <h2>${recipe.strMeal}</h2>
+                        <h3>Ingredients</h3>
+                        <ul>${ingredients}</ul>
+                        <h2>Instructions</h2>
+                        <p>${recipe.strInstructions}</p>
+                        <button class="button" onclick="saveRecipe(${recipe.idMeal})">Save Recipe</button>
+                    </div>
+                </div>
+            `;
+            // document.getElementById('recipe-details').innerHTML = output;
+            document.getElementById('recipe-container').innerHTML = details;
         });
 }
 
 function searchRecipes() {
     const searchTerm = document.getElementById('search').value.toLowerCase();
-    const recipes = document.querySelectorAll('.recipe');
+    const recipes = document.querySelectorAll('.card');
     recipes.forEach(recipe => {
         const title = recipe.querySelector('h3').textContent.toLowerCase();
         if (title.includes(searchTerm)) {
@@ -92,25 +126,36 @@ function saveRecipe(id) {
         savedRecipes.push(id);
         localStorage.setItem('savedRecipes', JSON.stringify(savedRecipes));
     }
+    document.getElementById('numberOfRecipes').innerHTML = savedRecipes.length
+    // console.log("recipe count : ", savedRecipes.length)
 }
 
 function displaySavedRecipes() {
     let savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
     let output = '';
+    let tbl = ''
     savedRecipes.forEach(id => {
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
             .then(response => response.json())
             .then(data => {
                 const recipe = data.meals[0];
-                output += `
-                    <div class="saved-recipe">
-                        <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}">
-                        <h3>${recipe.strMeal}</h3>
-                        <button onclick="removeRecipe(${id})">Remove</button>
-                        <button onclick="window.location.href='recipe.html?id=${id}'">View</button>
-                    </div>
-                `;
-                document.getElementById('saved-recipes').innerHTML = output;
+
+                tbl += `
+                    <tr>
+                        <td width="150">
+                            <div class="img-box">
+                                <img src="${recipe.strMealThumb}" class="img" alt="">
+                            </div>
+                        </td>
+                        <td width="150"><p style="font-size: 15px;">${recipe.strMeal}</p></td>
+                        <td width="70" onclick="removeRecipe(${id})">
+                            <i class="fa-solid fa-trash" style="color: red;"></i>
+                        </td>
+                    </tr>
+                `
+                
+                // document.getElementById('saved-recipes').innerHTML = output;
+                document.getElementById('root').innerHTML = tbl;
             });
     });
 }
@@ -122,6 +167,15 @@ function removeRecipe(id) {
     displaySavedRecipes();
 }
 
+
+function countSavedRecipes() {
+    let savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
+    console.log("Count : ", savedRecipes)
+    document.getElementById('numberOfRecipes').innerHTML = savedRecipes.length
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     HeaderComponent.loadNavbar();
+    let savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
+    document.getElementById('numberOfRecipes').innerHTML = savedRecipes.length
 });
